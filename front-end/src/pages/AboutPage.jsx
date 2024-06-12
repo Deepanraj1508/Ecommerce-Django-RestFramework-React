1c015cf7c0057c57c0e84518d77abaf30a2e70d0
Deepanraj K <deepanraj770848@gmail.com>
2024-06-12 18:03:52 +0530

    aboutPage

--- front-end/src/pages/AboutPage.jsx
+++ front-end/src/pages/AboutPage.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Footer, Navbar } from '../components';
+import groceryImage from '../assets/Grocery.jpg';
+import toyImage from '../assets/Toys.jpg';
+import jewelleryImage from '../assets/jewellery.jpg';
+import electronicsImage from '../assets/electronics.jpg';
 
 const AboutPage = () => {
   return (
@@ -26,31 +30,31 @@ const AboutPage = () => {
         <div className="row">
           <div className="col-md-3 col-sm-6 mb-3 px-3">
             <div className="card h-100">
-              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Men's Clothing" height={160} />
+              <img className="card-img-top img-fluid" src={groceryImage} alt="Grocery" height={160} />
               <div className="card-body">
-                <h5 className="card-title text-center">Men's Clothing</h5>
+                <h5 className="card-title text-center">Grocery</h5>
               </div>
             </div>
           </div>
           <div className="col-md-3 col-sm-6 mb-3 px-3">
             <div className="card h-100">
-              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/7679720/pexels-photo-7679720.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Women's Clothing" height={160} />
+              <img className="card-img-top img-fluid" src={toyImage} alt="Toys" height={160} />
               <div className="card-body">
-                <h5 className="card-title text-center">Women's Clothing</h5>
+                <h5 className="card-title text-center">Toys</h5>
               </div>
             </div>
           </div>
           <div className="col-md-3 col-sm-6 mb-3 px-3">
             <div className="card h-100">
-              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/1927259/pexels-photo-1927259.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Jewelry" height={160} />
+              <img className="card-img-top img-fluid"  src={jewelleryImage} alt="Jewelry" height={160} />
               <div className="card-body">
-                <h5 className="card-title text-center">Jewelry</h5>
+                <h5 className="card-title text-center">Jewellery</h5>
               </div>
             </div>
           </div>
           <div className="col-md-3 col-sm-6 mb-3 px-3">
             <div className="card h-100">
-              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Electronics" height={160} />
+              <img className="card-img-top img-fluid" src={electronicsImage} alt="Electronics" height={160} />
               <div className="card-body">
                 <h5 className="card-title text-center">Electronics</h5>
               </div>
