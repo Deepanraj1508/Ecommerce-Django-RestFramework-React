03034a3967df6e2f4999485d43531184d6b96a08
Deepanraj K <deepanraj770848@gmail.com>
2024-06-13 18:00:27 +0530

    fix

--- front-end/src/components/main.jsx
+++ front-end/src/components/main.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <div className="card bg-dark text-white border-0 mx-3">
           <img
             className="card-img img-fluid"
-            src="./assets/main.png"
+            src="./assets/main.png.jpg"
             alt="Card"
             height={500}
           />
